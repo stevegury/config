2659ad9d60f87083d9abb87e78a4ea6e0f8ce051
Steve Gury <stevegury@twitter.com>
2012-05-12 17:53:45 -0700

    update brew before installing package

--- brew-setup.sh
+++ brew-setup.sh
@@ -4,4 +4,5 @@
 /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
 
 # install packages
+brew update
 cat brew.list | xargs brew install
